cbfe03f9e535240cdf182291c381aec7dda71088
C4rbonsw1 <C4rbonsw1@yandex.com>
2019-04-26 15:08:06 +0100

    Add project files.

---
+++ TSLR-DeliveryMessagingSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TSLR_DeliveryMessagingSystem.MvcApplication" Language="C#" %>
---
+++ TSLR-DeliveryMessagingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TSLR_DeliveryMessagingSystem.Startup))]
+namespace TSLR_DeliveryMessagingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
